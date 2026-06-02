466faade4b4895a1f3e843c6f209e59f62cf434d
Aarti Jethaniya <jethaniya20aarti@gmail.com>
2026-06-02 10:29:02 +0530

    fixing links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1021,7 +1021,10 @@ customElements.define("recent-project", RecentProject);
 document.querySelectorAll('a[href^="#"]').forEach((link) => {
   link.addEventListener("click", (event) => {
     const id = link.getAttribute("href");
-    if (!id || id === "#") return;
+    if (!id || id === "#") {
+      event.preventDefault();
+      return;
+    }
 
     const target = document.querySelector(id);
     if (!target) return;
